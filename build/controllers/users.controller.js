1ebc0f39db5a2ed2379e8f8c389d1881433bbc29
maxivgg <maxivgg@gmail.com>
2021-06-21 13:01:39 -0300

    add babel runtime

--- build/controllers/users.controller.js
+++ build/controllers/users.controller.js
@@ -1,8 +1,6 @@
 import User from '../models/User.js';
 export var createUser = function createUser(req, res) {};
-export var getUsers = function getUsers(req, res) {
-  res.status(200).json();
-};
+export var getUsers = function getUsers(req, res) {};
 export var getUserById = function getUserById(req, res) {};
 export var updateUserById = function updateUserById(req, res) {};
 export var deleteUserById = function deleteUserById(req, res) {};
--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import cors from 'cors';
 import './database.js';
 app.use(cors());
-var PORT = process.env.PORT || 2000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   return console.log("Server started on port ".concat(PORT));
 });
