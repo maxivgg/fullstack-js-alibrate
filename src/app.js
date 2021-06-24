3a954a23c5deb072c783c7256139870377b55934
maxivgg <maxivgg@gmail.com>
2021-06-23 22:38:41 -0300

    change app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
+
 import booksRoutes from './routes/books.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import usersRoutes from './routes/users.routes.js';
@@ -10,7 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({origin: '*'}));
-app.use(express.static(path.join("./public/build")))
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', authRoutes);
