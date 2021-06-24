f91f231e4e52f42782cb7c8d5de9394ed8203c7d
maxivgg <maxivgg@gmail.com>
2021-06-23 22:52:03 -0300

    change package

--- src/public/src/utils/configs.ts
+++ src/public/src/utils/configs.ts
@@ -1,4 +1,6 @@
-export const URL =
-  window.location.hostname === "localhost"
+export const URL = "https://fullstack-js-alibrate.herokuapp.com/api/";
+/*  
+export const URL = window.location.hostname === "localhost"
     ? "http://localhost:2000/api/"
     : "https://fullstack-js-alibrate.herokuapp.com/api/";
+*/
