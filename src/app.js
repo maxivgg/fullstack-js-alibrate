f7090b31e8e461c32b60969bf0a504c0b751a02e
maxivgg <maxivgg@gmail.com>
2021-06-25 11:19:44 -0300

    change config cors heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import myLibraryRoutes from './routes/myLibrary.routes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: '*'}));
+app.use(cors());
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', authRoutes);
